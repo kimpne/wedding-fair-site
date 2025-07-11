6120316b735a03b4c861eaf1c3fb830f0278e950
jade <jadekim0105@gmail.com>
2025-07-11 14:31:21 +0900

    refactor: 클라이언트 fetch 구조 적용

--- pages/seoul-wedding-fair.js
+++ pages/seoul-wedding-fair.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 
-export default function 서울웨딩박람회() {
+export default function SeoulWeddingFair() {
   const [sheetData, setSheetData] = useState([]);
 
 useEffect(() => {
